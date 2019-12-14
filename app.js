49dff14c2585861a5ccefb7739532f31711f669b
ahmedragab96 <hamadar1996@gmail.com>
2019-12-14 17:50:29 +0200

    add mongo db and confgiure events model

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const graphHttp = require('express-graphql');
 const {
   buildSchema,
 } = require('graphql');
+const mongoose = require('mongoose');
+
+const Event = require('./models/event');
 
 const app = express();
 
@@ -11,12 +14,26 @@ app.use(bodyParser.json());
 
 app.use('/graphql', graphHttp({
   schema: buildSchema(`
+    type Event {
+      _id: ID
+      title: String!
+      description: String!
+      price: Float!
+      date: String!
+    }
+
+    input EventInput {
+      title: String!
+      description: String!
+      price: Float!
+    }
+
     type RootQuery {
-      events: [String!]!
+      events: [Event!]!
     }
 
     type RootMutation {
-      createEvent(name: String): String
+      createEvent(eventInput: EventInput): Event
     }
 
     schema {
@@ -26,14 +43,54 @@ app.use('/graphql', graphHttp({
   `),
   rootValue: {
     events: () => {
-      return ['Cinema', 'Sailing']
+      return Event
+        .find()
+        .then(events => {
+          return events.map(event => {
+            return {...event._doc};
+          })
+        })
+        .catch(error => {
+          console.log(error);
+        });
     },
     createEvent: (args) => {
-      const eventName = args.name;
-      return eventName;
+      const {
+        title,
+        description,
+        price
+      } = args.eventInput
+
+      // create event object
+      const event = new Event({
+        title: title,
+        description: description,
+        price: +price, 
+        date: new Date().toString(),
+      });
+
+      // save event in database
+      return event
+        .save()
+        .then( result =>{
+          return result;
+        }).catch(error => {
+          console.log(error);
+      });
     },
   },
   graphiql: true,
 }));
 
-app.listen(3000);
+mongoose
+  .connect(
+    `mongodb+srv://${process.env.MONGO_USER}:${
+      process.env.MONGO_PASSWORD
+    }@bookingapp-1puwi.mongodb.net/${
+      process.env.MONGO_DB
+    }?retryWrites=true&w=majority`
+  ).then(() => {
+    app.listen(3000);
+  }).catch(err => {
+    console.log(err);
+  });
